ce26e266b12333bcf81cfaa0c20d5c922ade7e63
Simonbolmut <bolmutsimon3@gmail.com>
2025-05-25 12:29:53 -0700

    Update week-two.py

--- week-two.py
+++ week-two.py
@@ -1,1 +1,25 @@
+# QUIZ 1 Create an empty list called my_list
+my_list = []
+
+#  QUIZ 2 Append elements 10, 20, 30, 40 to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#  QUIZ 3 Insert the value 15 at the second position (index 1)
+my_list.insert(1, 15)
+
+#  QUIZ 4 Extend my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+#  QUIZ 5 Remove the last element from my_list
+my_list.pop()
+
+#  QUIZ 6 Sort my_list in ascending order
+my_list.sort()
+
+#  QUIZ 7 Find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
 
